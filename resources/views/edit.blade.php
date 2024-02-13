4dc978cac73a5913d0803439611e600ae2ff66c7
AKThu <aungkhantthu002@gmail.com>
2024-02-13 23:17:15 +0630

    basic features of tasks. Add task, delete task, update task

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit task</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="flex flex-col items-center bg-[#eaeaea] text-[#121212] min-h-screen p-4">
+        <h1 class="text-3xl font-medium mb-4">Edit task</h1>
+        <div class="bg-[#fafafa] p-4 rounded-md drop-shadow-md w-fit flex flex-col items-center">
+            <form action="/todolist/{{ $task->id }}/edit"
+                  method="POST"
+                  class="flex flex-col">
+                @csrf
+                @method('PUT')
+                <div>
+                    <label for="task">Task</label>
+                    <input type="text" name="task" value="{{ $task->task }}"
+                           id="task" class="ml-2 p-2 border-2 border-gray-400 rounded-lg">
+                </div>
+                <button type="submit"
+                        class="mt-8 p-2 bg-sky-700 text-[#f2f2f2] rounded-lg hover:bg-sky-800 hover:drop-shadow-lg">
+                    Save changes
+                </button>
+            </form>
+        </div>
+        <a href="/todolist" class="mt-5 hover:text-sky-700"> >go back< </a>
+    </div>
+</body>
+
+</html>
--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -1,28 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo List</title>
     @vite('resources/css/app.css')
 </head>
+
 <body>
     <div class="flex flex-col items-center bg-[#eaeaea] text-[#121212] min-h-screen p-4">
         <h1 class="text-3xl font-medium mb-4">Todo List</h1>
+
         <div class="bg-[#fafafa] p-4 rounded-md drop-shadow-md w-4/5">
-            <a href="#">
-                <div class="flex flex-row justify-between py-1 px-3 my-3 w-full hover:bg-sky-700 hover:text-[#f2f2f2] hover:rounded-lg hover:drop-shadow-lg hover:p-2">
-                    <p>Drink water</p>
-                    <div class="flex flex-row">
-                        <a href="#delete"><div class="mx-4">edit</div></a>
-                        <a href="#delete"><div class="mx-4">mark as complete</div></a>
+            @foreach ($tasks as $task)
+                @if ($task->status === 0)
+                    <div
+                         class="flex flex-row justify-between py-1 px-3 my-3 w-full hover:bg-sky-700 hover:text-[#f2f2f2] hover:rounded-lg hover:drop-shadow-lg hover:p-2 {{ $task->status === 0 ?: 'line-through' }}">
+                        <p>{{ $task->task }}</p>
+                        <div class="flex flex-row">
+                            <form action="/todolist/{{ $task->id }}/update-status" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <button type="submit" class="mx-4">Mark as complete</button>
+                            </form>
+                            <form action="/todolist/{{ $task->id }}/edit">
+                                <button type="submit" class="mx-4">edit</button>
+                            </form>
+                            <form action="/todolist/{{ $task->id }}/delete" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="mx-4">delete</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </a>
-            <a href="#"><p class="py-1 px-3 my-3 w-full hover:bg-sky-700 hover:text-[#f2f2f2] hover:rounded-lg hover:drop-shadow-lg hover:p-2">Do exercise</p></a>
-            <a href="#"><p class="py-1 px-3 my-3 w-full hover:bg-sky-700 hover:text-[#f2f2f2] hover:rounded-lg hover:drop-shadow-lg hover:p-2">Read manga</p></a>
+                @endif
+            @endforeach
+            @foreach ($tasks as $task)
+                @if ($task->status === 1)
+                    <div
+                         class="flex flex-row justify-between py-1 px-3 my-3 w-full hover:bg-rose-700 hover:text-[#f2f2f2] hover:rounded-lg hover:drop-shadow-lg hover:p-2 line-through">
+                        <p>{{ $task->task }}</p>
+                        <div class="flex flex-row">
+                            <form action="/todolist/{{ $task->id }}/update-status" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <button type="submit" class="mx-4">Mark as uncomplete</button>
+                            </form>
+                            <form action="/todolist/{{ $task->id }}/delete" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="mx-4">delete</button>
+                            </form>
+                        </div>
+                    </div>
+                @endif
+            @endforeach
         </div>
+        <a href="/todolist/add">
+            <div class="mt-4 p-3 bg-[#fafafa] drop-shadow-md rounded-full hover:bg-sky-700 hover:text-[#f2f2f2]"> + Add
+                new task
+            </div>
+        </a>
     </div>
-    
+
 </body>
+
 </html>
