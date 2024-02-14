1a443364584b961c9991399ea39913d21794f1a6
AKThu <aungkhantthu002@gmail.com>
2024-02-14 12:22:54 +0630

    basic features edit, toggel complete/uncomplete, delete, add features added. This commit is clean

---
+++ resources/views/add.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add new task</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="flex flex-col items-center justify-center bg-[#eaeaea] text-[#121212] min-h-screen p-4">
+        <h1 class="text-3xl font-medium mb-8">Add new task</h1>
+        <div class="bg-[#fafafa] p-8 rounded-md drop-shadow-md w-2/5 flex flex-col items-center mb-8">
+            <form action="/todolist/add" method="post" class="flex flex-col items-center">
+                @csrf
+                <div>
+                    <label for="task" class="mr-3 text-xl">Task:</label>
+                    <input type="text" name="task" id="task"
+                           class="border-2 border-gray-300 rounded-lg px-2 py-1">
+                </div>
+                <button type="submit"
+                        class="mt-8 bg-sky-700 px-3 py-1 rounded-lg text-white w-full hover:bg-sky-800 hover:drop-shadow-lg">Add</button>
+            </form>
+        </div>
+        <a href="/todolist">
+            <div class="mb-10 hover:text-sky-700"> >Go back< </div>
+        </a>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,43 @@ Route::get('/', function () {
 });
 
 Route::get('/todolist', function () {
-    return view('todolist');
+    $tasks = Task::all();
+    return view('todolist', compact('tasks'));
+});
+
+Route::get('/todolist/add', function () {
+    return view('add');
+});
+
+Route::post('/todolist/add', function () {
+    $newTask = new Task();
+    $newTask->task = request('task');
+    $newTask->status = 0;
+    $newTask->save();
+    return redirect('/todolist');
+});
+
+Route::get('todolist/{id}/edit', function ($id) {
+    $task = Task::findOrFail($id);
+    return view('edit', compact('task'));
+});
+
+Route::put('todolist/{id}/edit', function ($id) {
+    $task = Task::findOrFail($id);
+    $task->task = request('task');
+    $task->save();
+    return redirect('/todolist');
+});
+
+Route::put('/todolist/{id}/update-status', function ($id) {
+    $task = Task::findOrFail($id);
+    $task->status = ($task->status === 0) ? 1 : 0;
+    $task->save();
+    return redirect('/todolist');
+});
+
+Route::delete('/todolist/{id}/delete', function ($id) {
+    $task = Task::findOrFail($id);
+    $task->delete();
+    return redirect('/todolist');
 });
