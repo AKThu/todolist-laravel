4d4d4a9de331cc83c78daa0cba5dc65f3d7e14db
AKThu <aungkhantthu002@gmail.com>
2024-02-14 10:45:57 +0630

    installed laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Task;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,44 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todolist', function () {
-    $tasks = Task::all();
-    return view('todolist', compact('tasks'));
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/todolist/add', function () {
-    return view('addTask');
-});
-
-Route::post('/todolist/add', function () {
-    $newTask = new Task();
-    $newTask->task = request('task');
-    $newTask->status = 0;
-    $newTask->save();
-    return redirect('/todolist');
-});
-
-Route::get('todolist/{id}/edit', function ($id) {
-    $task = Task::findOrFail($id);
-    return view('edit', compact('task'));
-});
-
-Route::put('todolist/{id}/edit', function ($id) {
-    $task = Task::findOrFail($id);
-    $task->task = request('task');
-    $task->save();
-    return redirect('/todolist');
-});
-
-Route::put('/todolist/{id}/update-status', function ($id) {
-    $task = Task::findOrFail($id);
-    $task->status = ($task->status === 0) ? 1 : 0;
-    $task->save();
-    return redirect('/todolist');
-});
-
-Route::delete('/todolist/{id}/delete', function ($id) {
-    $task = Task::findOrFail($id);
-    $task->delete();
-    return redirect('/todolist');
-});
+require __DIR__.'/auth.php';
